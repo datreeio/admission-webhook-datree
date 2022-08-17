7df42eee61163c06d01320ed64c899f681992862
Niv Weiss <niv@datree.io>
2022-08-18 00:31:00 +0300

    fix: keep only the relevant file for this repo

--- scripts/run-in-minikube.sh
+++
@@ -1,3 +0,0 @@
-bash ./scripts/deploy-in-minikube.sh
-
-kubectl apply -f ./internal/fixtures/webhook-demo.yaml
--- scripts/uninstall.sh
+++
@@ -1,9 +0,0 @@
-kubectl delete validatingwebhookconfiguration.admissionregistration.k8s.io/webhook-datree
-kubectl delete service/webhook-server -n datree
-kubectl delete deployment/webhook-server -n datree
-kubectl delete secret/webhook-server-tls -n datree
-kubectl delete clusterrolebinding/rolebinding:webhook-server-datree
-kubectl delete serviceaccount/webhook-server-datree -n datree
-kubectl delete clusterrole/webhook-server-datree
-kubectl label namespace kube-system admission.datree/validate-
-kubectl delete namespace/datree
