bcd910f16a18afacb92265399f0464b6339c48fe
Roy Hadad <roy@datree.io>
2022-09-22 20:01:53 +0300

    remove unecesery stuff

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -4,27 +4,13 @@ import (
 	"encoding/json"
 	"fmt"
 	"go.uber.org/zap"
-	v1 "k8s.io/api/admission/v1"
+	admission "k8s.io/api/admission/v1"
 )
 
 type Logger struct {
 	zapLogger *zap.SugaredLogger
 	requestId string
 }
-
-// enum iota
-
-type RequestDirection string
-
-const (
-	incoming = "dfdf" // a network error while validating the resource
-	Skipped  = "df"   // resource has been skipped, for example if its kind was not found and the user added the --ignore-missing-schemas flag
-)
-
-const (
-	RequestDirectionInbound  = "inbound"
-	RequestDirectionOutbound = "outbound"
-)
 
 type LogWithMetadata struct {
 	RequestId        string `json:"requestId"`
@@ -46,13 +32,12 @@ func (l *Logger) LogError(message string) {
 		// Structured context as loosely typed key-value pairs.
 		"requestId", l.requestId,
 	)
-	printLogsSeparator()
 }
 
-func (l *Logger) LogIncoming(admissionReview *v1.AdmissionReview) {
+func (l *Logger) LogIncoming(admissionReview *admission.AdmissionReview) {
 	l.logInfo(admissionReview, "incoming")
 }
-func (l *Logger) LogOutgoing(admissionReview *v1.AdmissionReview, isSkipped bool) {
+func (l *Logger) LogOutgoing(admissionReview *admission.AdmissionReview, isSkipped bool) {
 	l.logInfo(outgoingLog{
 		AdmissionReview: admissionReview,
 		IsSkipped:       isSkipped,
@@ -60,7 +45,7 @@ func (l *Logger) LogOutgoing(admissionReview *v1.AdmissionReview, isSkipped bool
 }
 
 type outgoingLog struct {
-	AdmissionReview *v1.AdmissionReview
+	AdmissionReview *admission.AdmissionReview
 	IsSkipped       bool
 }
 
@@ -74,29 +59,13 @@ func (l *Logger) logInfo(objectToLog any, requestDirection string) {
 		// Structured context as loosely typed key-value pairs.
 		"requestId", l.requestId,
 		"requestDirection", requestDirection)
-	printLogsSeparator()
 
 	// to dump all the logs from the last 72 hours, the user should run the following command:
 	// for podId in $(kubectl get pods -n datree --output name); do echo $(kubectl logs -n datree --since=72h $podId); done > datree-webhook-logs.txt
-	//
-	// when querying the logs, you can start with something like this javascript code:
-	// type "node" in the terminal to open the node console
-	/*
-		const fs = require('fs');
-		const items = fs.readFileSync('./datree-webhook-logs.txt', 'utf8')
-			.split("___DATREE_LOGS_SEPARATOR___")
-			.slice(0, -1)
-			.map(JSON.parse)
-			.filter((value) => value.requestDirection === 'incoming')
-			.map((value) => value.msg)
-		console.log(items)
-	*/
-
 }
 
 func LogUtil(msg string) {
 	fmt.Println(msg)
-	printLogsSeparator()
 }
 
 func (l *Logger) objectToJson(object any) string {
@@ -106,9 +75,4 @@ func (l *Logger) objectToJson(object any) string {
 		return ""
 	}
 	return string(result)
-}
-
-func printLogsSeparator() {
-	// this is needed in order to parse the logs correctly
-	fmt.Print("___DATREE_LOGS_SEPARATOR___")
 }
