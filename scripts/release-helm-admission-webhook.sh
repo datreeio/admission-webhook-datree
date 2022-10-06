a68af50e11a6c5b9ae9db8da3e44bb1ea32a7d1a
teselil <tzlil@datree.com>
2022-10-06 17:02:16 +0300

    add pr for main

--- scripts/release-helm-admission-webhook.sh
+++ scripts/release-helm-admission-webhook.sh
@@ -39,15 +39,12 @@ current_version=$(yq e '.version' ./charts/datree-admission-webhook/Chart.yaml)
 cecho "CYAN" "current version: $current_version"
 new_version=$(echo $current_version | awk -F. '{$NF = $NF + 1;} 1' | sed 's/ /./g')
 cecho "CYAN" "new version: $new_version"
-# update Chart.yaml
-yq e -i ".version = \"$new_version\"" ./charts/datree-admission-webhook/Chart.yaml
-
 
 #helm
 helm dependency build ./charts/datree-admission-webhook/
 git checkout gh-pages index.yaml
-helm repo index --url https://datreeio.github.io/admission-webhook-datree/ ./ --merge ./index.yaml
 helm package ./charts/datree-admission-webhook/ -d /tmp/
+helm repo index --url https://datreeio.github.io/admission-webhook-datree/ ./ --merge ./index.yaml
 mv index.yaml /tmp/
 cecho "GREEN" "helm package done"
 cecho "CYAN" "switch to temp branch to create PR"
@@ -63,7 +60,13 @@ git commit -m "feat: Release chart datree-admission-webhook-$new_version.tgz"
 git push --set-upstream origin "release-chart-$new_version"
 cecho "CYAN" "open PR"
 gh pr create --title "Release chart datree-admission-webhook-$new_version" --body "release chart $new_version" --base gh-pages --head release-chart-$new_version
-git checkout -
-cecho "CYAN" "switch back"
+git checkout main
+cecho "CYAN" "switch to main"
+# update Chart.yaml
+yq e -i ".version = \"$new_version\"" ./charts/datree-admission-webhook/Chart.yaml
+git add ./charts/datree-admission-webhook/Chart.yaml
+git commit -m "bump: Update Chart.yaml version"
+git push --set-upstream origin "update-chart-$new_version"
+gh pr create --title "Update Chart.yaml version" --body "update Chart.yaml $new_version" --base main --head update-chart-$new_version
 cecho "GREEN" "done"
 
