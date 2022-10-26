91bc0ab713c23892f3335b184527374014fe9a05
Roy Hadad <roy@datree.io>
2022-10-26 15:08:55 +0300

    refactor: refactor test cases for prerequisites

--- pkg/services/resourceFilterService_test.go
+++ pkg/services/resourceFilterService_test.go
@@ -10,60 +10,36 @@ import (
 	admission "k8s.io/api/admission/v1"
 )
 
-type shouldResourceBeValidatedTestCases struct {
-	testName           string
-	isSkipped          bool
-	admissionReviewReq string
-}
-
-//go:embed resourceFilterService_testFixtures/metadataNameMissing.json
-var metadataNameMissing string
-
-//go:embed resourceFilterService_testFixtures/deletedResource.json
-var deletedResource string
-
-//go:embed resourceFilterService_testFixtures/kindEvent.json
-var kindEvent string
-
-//go:embed resourceFilterService_testFixtures/kindGitRepository.json
-var kindGitRepository string
-
 //go:embed resourceFilterService_testFixtures/deploymentWithVariableFieldManager.json
 var deploymentWithVariableFieldManager string
 
-func TestShouldResourceBeValidated(t *testing.T) {
-	testCases := []shouldResourceBeValidatedTestCases{
-		{
-			testName:           "resource should be skipped because metadata name is missing",
-			isSkipped:          true,
-			admissionReviewReq: metadataNameMissing,
-		},
-		{
-			testName:           "resource should be skipped because it is deleted",
-			isSkipped:          true,
-			admissionReviewReq: deletedResource,
-		},
-		{
-			testName:           "resource should be skipped because kind is Event",
-			isSkipped:          true,
-			admissionReviewReq: kindEvent,
-		},
-		{
-			testName:           "resource should be skipped because kind is GitRepository",
-			isSkipped:          true,
-			admissionReviewReq: kindGitRepository,
-		},
-	}
-
+func TestPrerequisitesFilters(t *testing.T) {
 	server.ConfigMapScanningFilters.SkipList = []string{}
-	for _, testCase := range testCases {
-		admissionReviewReq, rootObject := extractAdmissionReviewReqAndRootObject(testCase.admissionReviewReq)
 
-		isResourceSkipped := !ShouldResourceBeValidated(admissionReviewReq, rootObject)
-		t.Run(testCase.testName, func(t *testing.T) {
-			assert.Equal(t, testCase.isSkipped, isResourceSkipped)
-		})
-	}
+	t.Run("resource should be skipped because resource is deleted", func(t *testing.T) {
+		admissionReviewReq, rootObject := extractAdmissionReviewReqAndRootObject(deploymentWithVariableFieldManager)
+		rootObject.Metadata.ManagedFields[0].Manager = "kubectl-client-side-apply"
+		rootObject.Metadata.DeletionTimestamp = "2021-01-01T00:00:00Z"
+		assert.Equal(t, false, ShouldResourceBeValidated(admissionReviewReq, rootObject))
+	})
+	t.Run("resource should be skipped because metadata name is missing", func(t *testing.T) {
+		admissionReviewReq, rootObject := extractAdmissionReviewReqAndRootObject(deploymentWithVariableFieldManager)
+		rootObject.Metadata.ManagedFields[0].Manager = "kubectl-client-side-apply"
+		rootObject.Metadata.Name = ""
+		assert.Equal(t, false, ShouldResourceBeValidated(admissionReviewReq, rootObject))
+	})
+	t.Run("resource should be skipped because kind is Event", func(t *testing.T) {
+		admissionReviewReq, rootObject := extractAdmissionReviewReqAndRootObject(deploymentWithVariableFieldManager)
+		rootObject.Metadata.ManagedFields[0].Manager = "kubectl-client-side-apply"
+		admissionReviewReq.Request.Kind.Kind = "Event"
+		assert.Equal(t, false, ShouldResourceBeValidated(admissionReviewReq, rootObject))
+	})
+	t.Run("resource should be skipped because kind is GitRepository", func(t *testing.T) {
+		admissionReviewReq, rootObject := extractAdmissionReviewReqAndRootObject(deploymentWithVariableFieldManager)
+		rootObject.Metadata.ManagedFields[0].Manager = "kubectl-client-side-apply"
+		admissionReviewReq.Request.Kind.Kind = "GitRepository"
+		assert.Equal(t, false, ShouldResourceBeValidated(admissionReviewReq, rootObject))
+	})
 }
 
 func TestConfigMapScanningFiltersValidation(t *testing.T) {
@@ -87,15 +63,6 @@ func TestConfigMapScanningFiltersValidation(t *testing.T) {
 		admissionReviewReq.Request.Kind.Kind = "non-skipped-kind"
 		assert.Equal(t, true, ShouldResourceBeValidated(admissionReviewReq, rootObject))
 	})
-}
-
-func extractAdmissionReviewReqAndRootObject(resource string) (*admission.AdmissionReview, RootObject) {
-	var admissionReviewReq *admission.AdmissionReview
-	if err := json.Unmarshal([]byte(resource), &admissionReviewReq); err != nil {
-		panic(err)
-	}
-	rootObject := getResourceRootObject(admissionReviewReq)
-	return admissionReviewReq, rootObject
 }
 
 func TestFieldManagersFilters(t *testing.T) {
@@ -162,4 +129,13 @@ func TestFieldManagersFilters(t *testing.T) {
 			assert.Equal(t, false, ShouldResourceBeValidated(admissionReviewReq, rootObject))
 		})
 	})
+}
+
+func extractAdmissionReviewReqAndRootObject(resource string) (*admission.AdmissionReview, RootObject) {
+	var admissionReviewReq *admission.AdmissionReview
+	if err := json.Unmarshal([]byte(resource), &admissionReviewReq); err != nil {
+		panic(err)
+	}
+	rootObject := getResourceRootObject(admissionReviewReq)
+	return admissionReviewReq, rootObject
 }
