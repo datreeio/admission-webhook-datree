98427af7a98baec5f184be8ba9ef28c781f54ff6
shmu3l <samuelsahala@gmail.com>
2022-10-31 10:03:09 +0200

    fix: working on

--- scripts/release-helm-admission-webhook.sh
+++ scripts/release-helm-admission-webhook.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+set -e
+set -o pipefail
 
 cecho(){
     RED="\033[0;31m"
@@ -31,45 +33,67 @@ fi
 
 }
 
+function verify_updated_main_branch(){
+    git checkout main
+    git pull origin main
+
+    if [ -n "$(git status --porcelain)" ]; then
+        cecho "RED" "Please commit your changes before running this script"
+        exit 1
+    fi
+}
+
 verify_command_exists
+verify_updated_main_branch
+
 
 # bump patch version Chart.yaml
-cecho "CYAN" "bump patch version"
+cecho "CYAN" "Bumping patch version..."
 current_version=$(yq e '.version' ./charts/datree-admission-webhook/Chart.yaml)
-cecho "CYAN" "current version: $current_version"
+cecho "CYAN" "Current version: $current_version"
 new_version=$(echo $current_version | awk -F. '{$NF = $NF + 1;} 1' | sed 's/ /./g')
-cecho "CYAN" "new version: $new_version"
+cecho "CYAN" "New version: $new_version"
 
 #helm
-helm dependency build ./charts/datree-admission-webhook/
-git checkout gh-pages index.yaml
-helm package ./charts/datree-admission-webhook/ --version=$new_version -d /tmp/
-helm repo index --url https://datreeio.github.io/admission-webhook-datree/ ./ --merge ./index.yaml
-mv index.yaml /tmp/
-cecho "GREEN" "helm package done"
-cecho "CYAN" "switch to temp branch to create PR"
-git checkout gh-pages
-git pull
-git checkout -b "release-chart-$new_version"
-mv "/tmp/datree-admission-webhook-$new_version.tgz" ./
-mv "/tmp/index.yaml" ./
-git add ./index.yaml
-git add ./datree-admission-webhook-$new_version.tgz
-git commit -m "feat: Release chart datree-admission-webhook-$new_version.tgz"
-git push --set-upstream origin "release-chart-$new_version"
-cecho "CYAN" "open PR"
-gh pr create --title "Release chart datree-admission-webhook-$new_version" --body "release chart $new_version" --base gh-pages --head release-chart-$new_version
-git checkout main
-git pull
-git checkout -b "update-chart-$new_version"
-cecho "CYAN" "switch to main"
-# update Chart.yaml
 yq e -i ".version = \"$new_version\"" ./charts/datree-admission-webhook/Chart.yaml
+cecho "GREEN" "Export helm package to /tmp"
+helm dependency build ./charts/datree-admission-webhook/
+helm package ./charts/datree-admission-webhook/ --version=$new_version -d /tmp/
+cecho "GREEN" "PR to main release-helm-chart-$new_version"
+git checkout -b "release-helm-chart-$new_version"
 git add ./charts/datree-admission-webhook/Chart.yaml
-git commit -m "bump: Update Chart.yaml version"
-git push --set-upstream origin "update-chart-$new_version"
-gh pr create --title "Update Chart.yaml version" --body "update Chart.yaml $new_version" --base main --head update-chart-$new_version
-git checkout main
-cecho "CYAN" "switch to main"
-cecho "GREEN" "done"
+git commit -m "Bump helm chart $new_version"
+git push origin "release-helm-chart-$new_version"
+cecho "GREEN" "Creating PR"
+gh pr create --title "bump version to $new_version" --body "bump version to $new_version" --base main --head "release-helm-chart-$new_version"
+
+
+# helm repo index --url https://datreeio.github.io/admission-webhook-datree/ ./ --merge ./index.yaml
+# mv index.yaml /tmp/
+# cecho "GREEN" "helm package done"
+# cecho "CYAN" "switch to temp branch to create PR"
+# git checkout gh-pages
+# git pull
+# git checkout -b "release-chart-$new_version"
+# mv "/tmp/datree-admission-webhook-$new_version.tgz" ./
+# mv "/tmp/index.yaml" ./
+# git add ./index.yaml
+# git add ./datree-admission-webhook-$new_version.tgz
+# git commit -m "feat: Release chart datree-admission-webhook-$new_version.tgz"
+# git push --set-upstream origin "release-chart-$new_version"
+# cecho "CYAN" "open PR"
+# gh pr create --title "Release chart datree-admission-webhook-$new_version" --body "release chart $new_version" --base gh-pages --head release-chart-$new_version
+# git checkout main
+# git pull
+# git checkout -b "update-chart-$new_version"
+# cecho "CYAN" "switch to main"
+# # update Chart.yaml
+# yq e -i ".version = \"$new_version\"" ./charts/datree-admission-webhook/Chart.yaml
+# git add ./charts/datree-admission-webhook/Chart.yaml
+# git commit -m "bump: Update Chart.yaml version"
+# git push --set-upstream origin "update-chart-$new_version"
+# gh pr create --title "Update Chart.yaml version" --body "update Chart.yaml $new_version" --base main --head update-chart-$new_version
+# git checkout main
+# cecho "CYAN" "switch to main"
+# cecho "GREEN" "done"
 
