332ecbd31bee1463670680e5e6e8a7cc59ff9852
Noaa Barki <noaabarki@Noaas-MacBook-Pro.local>
2022-11-15 16:59:58 +0200

    feat: check for webhook existence, wait for running pods. Refactor

---
+++ cmd/webhook-init/k8sclient.go
@@ -0,0 +1,166 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/datreeio/admission-webhook-datree/pkg/loggerUtil"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/watch"
+
+	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
+	_admissionregistrationv1 "k8s.io/client-go/kubernetes/typed/admissionregistration/v1"
+	corev1 "k8s.io/client-go/kubernetes/typed/core/v1"
+)
+
+type KubernetesClient interface {
+	CoreV1() corev1.CoreV1Interface
+	AdmissionregistrationV1() _admissionregistrationv1.AdmissionregistrationV1Interface
+}
+
+type k8sClient struct {
+	clientset KubernetesClient
+	config    struct {
+		namespace string
+	}
+}
+
+func newK8sClient(c KubernetesClient, ns string) *k8sClient {
+	return &k8sClient{
+		clientset: c,
+		config: struct{ namespace string }{
+			ns,
+		},
+	}
+}
+
+func (k *k8sClient) createPodWatcher(ctx context.Context, selector string) (watch.Interface, error) {
+	labelSelector := fmt.Sprint(selector)
+
+	opts := metav1.ListOptions{
+		TypeMeta:      metav1.TypeMeta{},
+		LabelSelector: labelSelector,
+		FieldSelector: "",
+	}
+
+	return k.clientset.CoreV1().Pods(k.config.namespace).Watch(ctx, opts)
+}
+
+func (k *k8sClient) waitPodRunning(ctx context.Context, selector string) error {
+	loggerUtil.Debugf("Creating watcher for POD with label:%s ...", selector)
+	watcher, err := k.createPodWatcher(ctx, selector)
+	if err != nil {
+		return err
+	}
+
+	loggerUtil.Debug("Watch out! Succuessfuly created watcher for PODs.")
+	defer watcher.Stop()
+
+	for {
+		select {
+		case event := <-watcher.ResultChan():
+			pod := event.Object.(*v1.Pod)
+
+			if pod.Status.Phase == v1.PodRunning {
+				if isPodReady(pod) {
+					loggerUtil.Debugf("The POD \"%s\" is running", selector)
+					return nil
+				}
+
+			}
+
+		case <-time.After(180 * time.Second):
+			loggerUtil.Debug("Exit from waitPodRunning for POD \"%s\" because the time is over")
+			return nil
+
+		case <-ctx.Done():
+			loggerUtil.Debugf("Exit from waitPodRunning for POD \"%s\" because the context is done", selector)
+			return nil
+		}
+	}
+}
+
+func isPodReady(pod *v1.Pod) bool {
+	checkPodReadyCondition := func(condition v1.PodCondition) bool {
+		return condition.Type == v1.PodReady && condition.Status == "True"
+	}
+
+	checkPodContainersCondition := func(condition v1.PodCondition) bool {
+		return condition.Type == v1.ContainersReady && condition.Status == "True"
+	}
+
+	return findIndex(checkPodReadyCondition, pod.Status.Conditions) > 0 && findIndex(checkPodContainersCondition, pod.Status.Conditions) > 0
+}
+
+type validationWebhookConfig struct {
+	name        string
+	caBundle    []byte
+	serviceName string
+	namesapce   string
+	selector    string
+}
+
+func (k *k8sClient) createValidationWebhookConfig(cfg *validationWebhookConfig) error {
+	path := "/validate"
+	sideEffects := admissionregistrationv1.SideEffectClassNone
+
+	validationWebhookConfig := &admissionregistrationv1.ValidatingWebhookConfiguration{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "datree-webhook",
+		},
+		Webhooks: []admissionregistrationv1.ValidatingWebhook{{
+			Name: "webhook-server.datree.svc",
+			ClientConfig: admissionregistrationv1.WebhookClientConfig{
+				CABundle: cfg.caBundle, // CA bundle created earlier
+				Service: &admissionregistrationv1.ServiceReference{
+					Name:      cfg.serviceName, // datree-webhook-server
+					Namespace: "datree",
+					Path:      &path,
+				},
+			},
+			Rules: []admissionregistrationv1.RuleWithOperations{{Operations: []admissionregistrationv1.OperationType{
+				admissionregistrationv1.Create,
+				admissionregistrationv1.Update,
+			},
+				Rule: admissionregistrationv1.Rule{
+					APIGroups:   []string{"*"},
+					APIVersions: []string{"*"},
+					Resources:   []string{"*"},
+				},
+			}},
+			SideEffects:             &sideEffects,
+			AdmissionReviewVersions: []string{"v1", "v1beta1"},
+			TimeoutSeconds:          &[]int32{30}[0],
+			NamespaceSelector: &metav1.LabelSelector{
+				MatchExpressions: []metav1.LabelSelectorRequirement{
+					{ // only validate pods in namespaces with the label "admission.datree/validate"
+						Key:      cfg.selector,
+						Operator: metav1.LabelSelectorOpDoesNotExist,
+					},
+				},
+			},
+		}},
+	}
+
+	_, err := k.clientset.AdmissionregistrationV1().ValidatingWebhookConfigurations().Create(context.Background(), validationWebhookConfig, metav1.CreateOptions{})
+	return err
+}
+
+func (k *k8sClient) deleteExistingValidationAdmissionWebhook(name string) error {
+	vw := k.getValidationAdmissionWebhook("datree-webhook")
+	if vw != nil {
+		return k.clientset.AdmissionregistrationV1().ValidatingWebhookConfigurations().Delete(context.Background(), name, metav1.DeleteOptions{})
+	}
+	return nil
+}
+
+func (k *k8sClient) getValidationAdmissionWebhook(name string) *admissionregistrationv1.ValidatingWebhookConfiguration {
+	vw, err := k.clientset.AdmissionregistrationV1().ValidatingWebhookConfigurations().Get(context.Background(), name, metav1.GetOptions{})
+	if vw.Name == name && err == nil {
+		return vw
+	}
+
+	return nil
+}
--- cmd/webhook-init/main.go
+++ cmd/webhook-init/main.go
@@ -5,83 +5,89 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"time"
 
 	"github.com/datreeio/admission-webhook-datree/pkg/loggerUtil"
-
-	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	ctrl "sigs.k8s.io/controller-runtime"
 )
 
 func main() {
-	certPath := filepath.Join(`/etc/webhook/certs`, `ca-bundle.pem`)
-	caPEM, _ := os.ReadFile(certPath)
-
-	time.Sleep(180 * time.Second)
-	loggerUtil.Log("Sleep Over.....")
-
-	err := createValidationWebhookConfig(caPEM)
-
+	// create validation webhook config
+	datreeValidationWebhookConfig, err := castDatreeValidationWebhookConfig()
 	if err != nil {
-		loggerUtil.Log(fmt.Sprintf("failed to create validation webhook config, err: %v", err))
-	} else {
-		loggerUtil.Log("created validating webhook configuration")
+		loggerUtil.Logf("failed cast webhook configuration, err: %v", err)
+		return
 	}
+
+	// create k8s client
+	kubeClient, err := kubernetes.NewForConfig(ctrl.GetConfigOrDie())
+	if err != nil {
+		loggerUtil.Logf("failed to set k8s go -client, err: %v", err)
+		return
+	}
+	k8sClient := newK8sClient(kubeClient, "datree")
+
+	// wait for webhook-server pods to be ready
+	err = k8sClient.waitPodRunning(context.Background(), "app=datree-webhook-server")
+	if err != nil {
+		loggerUtil.Logf("failed to wait for pods, err: %v", err)
+		return
+	}
+
+	// create validation webhook, if one already exists delete before creating
+	loggerUtil.Log("creating ValidatingWebhook...")
+	err = k8sClient.deleteExistingValidationAdmissionWebhook("datree-webhook")
+	if err != nil {
+		loggerUtil.Logf("failed to delete existed validation webhook config, err: %v", err)
+		return
+	}
+
+	err = k8sClient.createValidationWebhookConfig(datreeValidationWebhookConfig)
+	if err != nil {
+		loggerUtil.Logf("failed to create validation webhook config, err: %v", err)
+	}
+
+	loggerUtil.Log("Horray! Succesfully initiaded datree validation admission webhook.")
+	os.Exit(0)
 }
 
-func createValidationWebhookConfig(caBundle []byte) error {
-	config := ctrl.GetConfigOrDie()
-	kubeClient, err := kubernetes.NewForConfig(config)
+func castDatreeValidationWebhookConfig() (*validationWebhookConfig, error) {
+	webhookServiceName, isFound := os.LookupEnv("WEBHOOK_SERVICE")
+	if !isFound {
+		return nil, fmt.Errorf("required environment variable WEBHOOK_SERVICE is missing")
+	}
+
+	webhookNamespace, isFound := os.LookupEnv("WEBHOOK_NAMESPACE")
+	if !isFound {
+		return nil, fmt.Errorf("required environment variable WEBHOOK_NAMESPACE is missing")
+	}
+
+	webhookSelector, isFound := os.LookupEnv("WEBHOOK_SELECTOR")
+	if !isFound {
+		return nil, fmt.Errorf("required environment variable WEBHOOK_SELECTOR is missing")
+	}
+
+	// read CA bundle
+	caBundle, err := readCABundle()
 	if err != nil {
-		return err // panic("failed to set go -client")
+		return nil, fmt.Errorf("failed to read caBundle, err: %v", err)
 	}
 
-	path := "/validate"
-	sideEffects := admissionregistrationv1.SideEffectClassNone
+	return &validationWebhookConfig{
+		name:        "datree-webhook",
+		serviceName: webhookServiceName,
+		namesapce:   webhookNamespace,
+		caBundle:    caBundle,
+		selector:    webhookSelector,
+	}, nil
+}
 
-	validationWebhookConfig := &admissionregistrationv1.ValidatingWebhookConfiguration{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "datree-webhook",
-		},
-		Webhooks: []admissionregistrationv1.ValidatingWebhook{{
-			Name: "webhook-server.datree.svc",
-			ClientConfig: admissionregistrationv1.WebhookClientConfig{
-				CABundle: caBundle, // CA bundle created earlier
-				Service: &admissionregistrationv1.ServiceReference{
-					Name:      "datree-webhook-server", // datree-webhook-server
-					Namespace: "datree",
-					Path:      &path,
-				},
-			},
-			Rules: []admissionregistrationv1.RuleWithOperations{{Operations: []admissionregistrationv1.OperationType{
-				admissionregistrationv1.Create,
-				admissionregistrationv1.Update,
-			},
-				Rule: admissionregistrationv1.Rule{
-					APIGroups:   []string{"*"},
-					APIVersions: []string{"*"},
-					Resources:   []string{"*"},
-				},
-			}},
-			SideEffects:             &sideEffects,
-			AdmissionReviewVersions: []string{"v1", "v1beta1"},
-			TimeoutSeconds:          &[]int32{30}[0],
-			NamespaceSelector: &metav1.LabelSelector{
-				MatchExpressions: []metav1.LabelSelectorRequirement{
-					{ // only validate pods in namespaces with the label "admission.datree/validate"
-						Key:      "admission.datree/validate",
-						Operator: metav1.LabelSelectorOpDoesNotExist,
-					},
-				},
-			},
-		}},
+func readCABundle() ([]byte, error) {
+	certPath := filepath.Join(`/etc/webhook/certs`, `ca-bundle.pem`)
+	caPEM, err := os.ReadFile(certPath)
+	if err != nil {
+		return nil, err
 	}
 
-	if _, err = kubeClient.AdmissionregistrationV1().ValidatingWebhookConfigurations().Create(context.Background(), validationWebhookConfig, metav1.CreateOptions{}); err != nil {
-		return err
-	}
-
-	return nil
+	return caPEM, nil
 }
