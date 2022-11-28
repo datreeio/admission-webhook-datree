f488ee997d636901aae5aebee624f7de57df0619
Noaa Barki <noaabarki@Noaas-MacBook-Pro.local>
2022-11-28 13:00:36 +0200

    feat: support multiple binaries: webhook-init, cert-generator, webhook-server
    fix: updated Makefile. Support automatic deployment of awsmp chart
    
    fix: added code changes from main branch

---
+++ cmd/init-webhook/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	k8sclient "github.com/datreeio/admission-webhook-datree/cmd/init-webhook/k8s-client"
+	webhookinfo "github.com/datreeio/admission-webhook-datree/cmd/init-webhook/webhook-info"
+	"github.com/datreeio/admission-webhook-datree/pkg/logger"
+	admissionregistrationV1 "k8s.io/api/admissionregistration/v1"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/watch"
+)
+
+func main() {
+	k8sClient := k8sclient.New(nil)
+	if k8sClient == nil {
+		logger.LogUtil("failed to set k8s go -client, err")
+	}
+
+	err := InitWebhook(k8sClient)
+	if err != nil {
+		logger.LogUtil(fmt.Sprintf("failed to init webhook, err: %v", err))
+	}
+	logger.LogUtil("horray! succesfully created datree validating admission webhook")
+
+	// wait forever to prevent the container from restrating
+	waitForever()
+}
+
+type k8sClientInterface interface {
+	CreateValidatingWebhookConfiguration(namespace string, cfg *k8sclient.ValidatingWebhookOpts) (*admissionregistrationV1.ValidatingWebhookConfiguration, error)
+	DeleteExistingValidatingWebhook(name string) error
+	WaitUntilPodsAreRunning(ctx context.Context, namespace string, selector string, replicas int) error
+	GetValidatingWebhookConfiguration(name string) *admissionregistrationV1.ValidatingWebhookConfiguration
+	CreatePodWatcher(ctx context.Context, namespace string, selector string) (watch.Interface, error)
+	IsPodReady(pod *v1.Pod) bool
+}
+
+func InitWebhook(k8sClient k8sClientInterface) error {
+	err := k8sClient.DeleteExistingValidatingWebhook("datree-webhook")
+	if err != nil {
+		logger.LogUtil(fmt.Sprintf("failed to delete existed validation webhook config, err: %v", err))
+		return err
+	}
+
+	err = k8sClient.WaitUntilPodsAreRunning(context.Background(), webhookinfo.GetWebhookNamespace(), webhookinfo.GetWebhookPodsSelector(), webhookinfo.GetWebhookServerReplicas())
+	if err != nil {
+		logger.LogUtil(fmt.Sprintf("failed to wait for pods, err: %v", err))
+		return err
+	}
+
+	caBundle, _ := webhookinfo.GetWebhookCABundle()
+	_, err = k8sClient.CreateValidatingWebhookConfiguration(webhookinfo.GetWebhookNamespace(), &k8sclient.ValidatingWebhookOpts{
+		MetaName:    "datree-webhook",
+		ServiceName: webhookinfo.GetWebhookServiceName(),
+		CaBundle:    caBundle,
+		Selector:    webhookinfo.GetWebhookSelector(),
+		WebhookName: "webhook-server.datree.svc",
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func waitForever() {
+	select {}
+}
---
+++ cmd/init-webhook/webhook-info/webhookInfo_test.go
@@ -0,0 +1,205 @@
+package webhookinfo
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type Condition[T any] func(actual T) bool
+
+type ExpectedCondition[T any] struct {
+	condition Condition[T]
+	message   string
+}
+
+func TestGetWebhookServerReplicas(t *testing.T) {
+	type testCase struct {
+		envVarValue string
+		expected    ExpectedCondition[int]
+	}
+
+	tests := map[string]*testCase{
+		"should return 2 when replicas is not set": {
+			envVarValue: "",
+			expected: ExpectedCondition[int]{
+				condition: func(actual int) bool {
+					return actual == 2
+				},
+				message: "expected replicas to be 2, got %d",
+			},
+		},
+		"should return 2 when replicas is set to 2": {
+			envVarValue: "2",
+			expected: ExpectedCondition[int]{
+				condition: func(actual int) bool {
+					return actual == 2
+				},
+				message: "expected replicas to be 2, got %d",
+			},
+		},
+		"should return 1 when replicas is set to 1": {
+			envVarValue: "1",
+			expected: ExpectedCondition[int]{
+				condition: func(actual int) bool {
+					return actual == 1
+				},
+				message: "expected replicas to be 1, got %d",
+			},
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			os.Setenv("WEBHOOK_SERVER_REPLICAS", test.envVarValue)
+			replicas := GetWebhookServerReplicas()
+			assert.True(t, test.expected.condition(replicas), test.expected.message, replicas)
+		})
+	}
+}
+
+func TestGetWebhookSelector(t *testing.T) {
+	type testCase struct {
+		envVarValue string
+		expected    ExpectedCondition[string]
+	}
+
+	tests := map[string]*testCase{
+		"should 'admission.datree/validate' when replicas is not set": {
+			envVarValue: "",
+			expected: ExpectedCondition[string]{
+				condition: func(actual string) bool {
+					return actual == "admission.datree/validate"
+				},
+				message: "expected selector to be 'admission.datree/validate', got %d",
+			},
+		},
+		"should return 'datree/validate' when selector is set to 'datree/validate'": {
+			envVarValue: "datree/validate",
+			expected: ExpectedCondition[string]{
+				condition: func(actual string) bool {
+					return actual == "datree/validate"
+				},
+				message: "expected selector to be 'datree/validate', got %d",
+			},
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			os.Setenv("WEBHOOK_SELECTOR", test.envVarValue)
+			selector := GetWebhookSelector()
+			assert.True(t, test.expected.condition(selector), test.expected.message, selector)
+		})
+	}
+}
+
+func TestGetWebhookNamespace(t *testing.T) {
+	type testCase struct {
+		envVarValue string
+		expected    ExpectedCondition[string]
+	}
+
+	tests := map[string]*testCase{
+		"should return datree namespace when namespace is not set": {
+			envVarValue: "",
+			expected: ExpectedCondition[string]{
+				condition: func(actual string) bool {
+					return actual == "datree"
+				},
+				message: "expected namespace to be 'datree', got %d",
+			},
+		},
+		"should return default namespace when namespace is set to default": {
+			envVarValue: "default",
+			expected: ExpectedCondition[string]{
+				condition: func(actual string) bool {
+					return actual == "default"
+				},
+				message: "expected namesapce to be 'default', got %d",
+			},
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			os.Setenv("WEBHOOK_NAMESPACE", test.envVarValue)
+			ns := GetWebhookNamespace()
+			assert.True(t, test.expected.condition(ns), test.expected.message, ns)
+		})
+	}
+}
+
+func TestGetWebhookServiceName(t *testing.T) {
+	type testCase struct {
+		envVarValue string
+		expected    ExpectedCondition[string]
+	}
+
+	tests := map[string]*testCase{
+		"should return 'datree-webhook-server' when service name is not set": {
+			envVarValue: "",
+			expected: ExpectedCondition[string]{
+				condition: func(actual string) bool {
+					return actual == "datree-webhook-server"
+				},
+				message: "expected service name to be 'datree-webhook-server', got %d",
+			},
+		},
+		"should return 'datree-service' when selector is set to 'datree-service'": {
+			envVarValue: "datree-service",
+			expected: ExpectedCondition[string]{
+				condition: func(actual string) bool {
+					return actual == "datree-service"
+				},
+				message: "expected service name to be 'datree-service', got %d",
+			},
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			os.Setenv("WEBHOOK_SERVICE", test.envVarValue)
+			serviceName := GetWebhookServiceName()
+			assert.True(t, test.expected.condition(serviceName), test.expected.message, serviceName)
+		})
+	}
+
+}
+
+func TestGetWebhookServerPodSelector(t *testing.T) {
+	type testCase struct {
+		envVarValue string
+		expected    ExpectedCondition[string]
+	}
+
+	tests := map[string]*testCase{
+		"should return 'app=app=datree-webhook-server' when pod selector is not set": {
+			envVarValue: "",
+			expected: ExpectedCondition[string]{
+				condition: func(actual string) bool {
+					return actual == "app=datree-webhook-server"
+				},
+				message: "expected pod selector to be 'app=datree-webhook-server', got %d",
+			},
+		},
+		"should return 'app=test' when pod selector is set to 'app=test'": {
+			envVarValue: "app=test",
+			expected: ExpectedCondition[string]{
+				condition: func(actual string) bool {
+					return actual == "app=test"
+				},
+				message: "expected pod selector to be 'app=test', got %d",
+			},
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			os.Setenv("WEBHOOK_POD_SELECTOR", test.envVarValue)
+			podSelector := GetWebhookPodsSelector()
+			assert.True(t, test.expected.condition(podSelector), test.expected.message, podSelector)
+		})
+	}
+}
