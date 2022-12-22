653888aad8bef72a3af4f34f9bac2c82b2b277df
Yishay Mendelsohn <myishay@gmail.com>
2022-12-22 10:53:33 +0200

    fix: add troubleshoot script (#235)
    * fix: add troubleshoot script
    
    * refactor: move script to scripts dir
    
    * fix: get cluster uid
    
    * refactor: change order of commands

---
+++ scripts/troubleshoot.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e
+
+# Colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;36m'
+PLAIN='\033[0m'
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+
+
+# Run command and show stdout and stderr
+run_command() {
+  echo -e "${BLUE}Running: ${YELLOW}$@${PLAIN}"
+  "$@"
+  echo ""
+}
+
+
+run_command kubectl version
+
+run_command kubectl config current-context
+
+run_command kubectl get ns
+
+run_command helm list -n datree
+
+run_command kubectl get all -n datree
+
+run_command kubectl get ns kube-system -o jsonpath='{.metadata.uid}'
+
+# get image version of datree scan-job
+run_command kubectl get job.batch/scan-job -n datree -o jsonpath='{.spec.template.spec.containers[0].image}'
+
+run_command kubectl logs job.batch/scan-job -n datree
+
+run_command kubectl get validatingwebhookconfigurations
+
+run_command kubectl get mutatingwebhookconfigurations
+
