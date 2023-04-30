afa1e29f3fd334cd1fb35a0678d2e206c292922e
shmu3l <samuelsahala@gmail.com>
2023-04-30 12:45:09 +0300

    fix: uninstall script

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -1,9 +1,14 @@
+#!/bin/bash
+
+NAMESPACE=datree
+
 kubectl delete validatingwebhookconfiguration.admissionregistration.k8s.io/webhook-datree
-kubectl delete service/webhook-server -n datree
-kubectl delete deployment/webhook-server -n datree
-kubectl delete secret/webhook-server-tls -n datree
-kubectl delete clusterrolebinding/rolebinding:webhook-server-datree
-kubectl delete serviceaccount/webhook-server-datree -n datree
-kubectl delete clusterrole/webhook-server-datree
+kubectl delete service/webhook-server -n $NAMESPACE
+kubectl delete deployment/webhook-server -n $NAMESPACE
+kubectl delete deployment/datree-cluster-scanner-server -n $NAMESPACE
+kubectl delete secret/webhook-server-tls -n $NAMESPACE
+kubectl delete clusterrolebinding --all -n $NAMESPACE
+kubectl delete serviceaccounts --all -n $NAMESPACE
+kubectl delete clusterrole --all -n $NAMESPACE
 kubectl label namespace kube-system admission.datree/validate-
 kubectl delete namespace/datree
