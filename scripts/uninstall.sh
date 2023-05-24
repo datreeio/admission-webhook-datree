dfddb597076116edfc10eea13ca5788dffd0f822
Roy Hadad <roy@datree.io>
2023-05-24 13:50:37 +0300

    add namespace var

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -9,10 +9,12 @@ kubectl delete deployment/datree-cluster-scanner-server -n $NAMESPACE
 kubectl delete secret/webhook-server-tls -n $NAMESPACE
 kubectl delete clusterrolebinding/datree-webhook-server-cluster-role
 kubectl delete clusterrolebinding/cluster-scanner-role-binding
-kubectl delete serviceaccount/datree-webhook-server -n datree
-kubectl delete serviceaccount/cluster-scanner-service-account -n datree
+kubectl delete serviceaccount/datree-webhook-server -n $NAMESPACE
+kubectl delete serviceaccount/cluster-scanner-service-account -n $NAMESPACE
 kubectl delete clusterrole/datree-webhook-server-cluster-role
 kubectl delete clusterrole/cluster-scanner-role
+
+kubectl delete job/datree-wait-server-ready-hook-post-install -n $NAMESPACE
 
 kubectl label namespace kube-system admission.datree/validate-
 kubectl delete namespace/datree
