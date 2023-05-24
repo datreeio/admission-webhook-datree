9da6c27a00aabc232e97b9bb6ed35caaf2365102
Roy Hadad <roy@datree.io>
2023-05-24 13:54:51 +0300

    done

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -2,19 +2,24 @@
 
 NAMESPACE=datree
 
+# webhook
 kubectl delete validatingwebhookconfiguration.admissionregistration.k8s.io/datree-webhook
 kubectl delete service/datree-webhook-server -n $NAMESPACE
 kubectl delete deployment/datree-webhook-server -n $NAMESPACE
-kubectl delete deployment/datree-cluster-scanner-server -n $NAMESPACE
-kubectl delete secret/webhook-server-tls -n $NAMESPACE
 kubectl delete clusterrolebinding/datree-webhook-server-cluster-role
-kubectl delete clusterrolebinding/cluster-scanner-role-binding
 kubectl delete serviceaccount/datree-webhook-server -n $NAMESPACE
-kubectl delete serviceaccount/cluster-scanner-service-account -n $NAMESPACE
 kubectl delete clusterrole/datree-webhook-server-cluster-role
+kubectl delete secret/webhook-server-tls -n $NAMESPACE
+
+# scanner
+kubectl delete deployment/datree-cluster-scanner-server -n $NAMESPACE
+kubectl delete clusterrolebinding/cluster-scanner-role-binding
+kubectl delete serviceaccount/cluster-scanner-service-account -n $NAMESPACE
 kubectl delete clusterrole/cluster-scanner-role
 
+# misc
 kubectl delete job/datree-wait-server-ready-hook-post-install -n $NAMESPACE
-
 kubectl label namespace kube-system admission.datree/validate-
+
+# namespace
 kubectl delete namespace/datree
