11f910e250494c2d79efaa1bf1fd6f5d36328de1
Roy Hadad <roy@datree.io>
2023-05-24 13:26:35 +0300

    fix: uninstall script

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -7,8 +7,12 @@ kubectl delete service/webhook-server -n $NAMESPACE
 kubectl delete deployment/webhook-server -n $NAMESPACE
 kubectl delete deployment/datree-cluster-scanner-server -n $NAMESPACE
 kubectl delete secret/webhook-server-tls -n $NAMESPACE
-kubectl delete clusterrolebinding --all -n $NAMESPACE
-kubectl delete serviceaccounts --all -n $NAMESPACE
-kubectl delete clusterrole --all -n $NAMESPACE
+kubectl delete clusterrolebinding/rolebinding:webhook-server-datree
+kubectl delete clusterrolebinding/rolebinding:cluster-scanner-role-binding
+kubectl delete serviceaccount/webhook-server-datree -n datree
+kubectl delete serviceaccount/cluster-scanner-service-account -n datree
+kubectl delete clusterrole/webhook-server-datree
+kubectl delete clusterrole/cluster-scanner-role
+
 kubectl label namespace kube-system admission.datree/validate-
 kubectl delete namespace/datree
