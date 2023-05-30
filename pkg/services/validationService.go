1ff20b09d0a20c4d8f373fefb45bd3e6e994546a
Roy Hadad <roy@datree.io>
2023-05-30 11:41:58 +0300

    remove todo

--- pkg/services/validationService.go
+++ pkg/services/validationService.go
@@ -224,7 +224,7 @@ func (vs *ValidationService) Validate(admissionReviewReq *admission.AdmissionRev
 		}
 	}
 
-	clusterRequestMetadata := getClusterRequestMetadata(cliEvaluationId, token, false, allowed, resourceKind, resourceName, managers, clusterK8sVersion, "PLACEHOLDER_POLICY_NAME", namespace, server.ConfigMapScanningFilters) // TODO use the real policy name
+	clusterRequestMetadata := getClusterRequestMetadata(cliEvaluationId, token, false, allowed, resourceKind, resourceName, managers, clusterK8sVersion, vs.State.GetPolicyName(), namespace, server.ConfigMapScanningFilters)
 	vs.saveRequestMetadataLogInAggregator(clusterRequestMetadata)
 	return ParseEvaluationResponseIntoAdmissionReview(admissionReviewReq.Request.UID, allowed, msg, *warningMessages), false
 }
